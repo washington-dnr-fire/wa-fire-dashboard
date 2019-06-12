2b502018b64b13a68586ae06601dce373821af5b
Davis <kdav490@dnr.wa.gov>
2019-06-12 12:03:51 -0700

    index page, view updates for no records

--- fire_intel/views.py
+++ fire_intel/views.py
@@ -5,9 +5,10 @@ from .models import IntelReport
 # Create your views here.
 def index(request):
     template = loader.get_template('fire_intel/index.html')
-    latest_intel_report = IntelReport.objects.latest("date_of_report")
-    updated_date_txt = latest_intel_report.date_of_report.strftime("%m/%d/%Y, %H:%M:%S")
-    context = {
+    if IntelReport.objects.count() > 0:
+        latest_intel_report = IntelReport.objects.latest("date_of_report")
+        updated_date_txt = latest_intel_report.date_of_report.strftime("%m/%d/%Y, %H:%M:%S")
+        context = {
         'preparedness_level_national':latest_intel_report.preparedness_level_national,
         'preparedness_level_nw':latest_intel_report.preparedness_level_nw,
         'type_1_teams_assigned':latest_intel_report.type_1_teams_assigned,
@@ -52,12 +53,61 @@ def index(request):
         'in_region_ang':latest_intel_report.in_region_ang,
         'updated_date_txt': updated_date_txt,
     }
+    else:
+        context = {
+        'preparedness_level_national':0,
+        'preparedness_level_nw':0,
+        'type_1_teams_assigned':0,
+        'type_2_teams_assigned':0,
+        'wa_large_fires':0,
+        'dnr_ia_fires':0,
+        'ne_committed_engines':0,
+        'ne_committed_crews':0,
+        'ne_available_engines':0,
+        'ne_available_crews':0,
+        'se_committed_engines':0,
+        'se_committed_crews':0,
+        'se_available_engines':0,
+        'se_available_crews':0,
+        'nw_committed_engines':0,
+        'nw_committed_crews':0,
+        'nw_available_engines':0,
+        'nw_available_crews':0,
+        'pc_committed_engines':0,
+        'pc_committed_crews':0,
+        'pc_available_engines':0,
+        'pc_available_crews':0,
+        'oly_committed_engines':0,
+        'oly_committed_crews':0,
+        'oly_available_engines':0,
+        'oly_available_crews':0,
+        'sps_committed_engines':0,
+        'sps_committed_crews':0,
+        'sps_available_engines':0,
+        'sps_available_crews':0,
+        'westside_rotors':0,
+        'westside_firebosses':0,
+        'westside_vlat':0,
+        'westside_ang':0,
+        'eastside_rotors':0,
+        'eastside_firebosses':0,
+        'eastside_vlat':0,
+        'eastside_ang':0,
+        'in_region_rotors':0,
+        'in_region_firebosses':0,
+        'in_region_vlat':0,
+        'in_region_ang':0,
+        'updated_date_txt': "No reports available",
+    }
     return HttpResponse(template.render(context, request))
 
 def profile(request):
     template = loader.get_template('fire_intel/profile.html')
-    latest_intel_report = IntelReport.objects.latest("date_of_report")
-    updated_date_txt = latest_intel_report.date_of_report.strftime("%m/%d/%Y, %H:%M:%S")
+    if IntelReport.objects.count() > 0:
+        latest_intel_report = IntelReport.objects.latest("date_of_report")
+        updated_date_txt = latest_intel_report.date_of_report.strftime("%m/%d/%Y, %H:%M:%S")
+    else:
+        updated_date_txt = "No reports available"
     if request.user.is_authenticated:
         user = request.user.username
         first_name = request.user.first_name
@@ -72,4 +122,7 @@ def profile(request):
         }
         return HttpResponse(template.render(context, request))
     else:
-        return HttpResponse(template.render({}, request))
+        context = {
+            'updated_date_txt': updated_date_txt,
+        }
+        return HttpResponse(template.render(context, request))

65254426ea06396b674a127ce7d449106fd150e4
Kirk Davis <kirk5davis@gmail.com>
2020-06-26 12:10:32 -0700

    update NWS weather url

--- project_space_crab/static/js/main.js
+++ project_space_crab/static/js/main.js
@@ -148,8 +148,7 @@ $(function() {
     NWS_warnings.bindPopup(function(evt) {
         var t = moment.utc(evt.feature.properties['expiration']).local().fromNow();
         var s = moment.utc(evt.feature.properties['issuance']).local().fromNow();
-        // This is a ghetto fix until the NWS decides to do things the right way
-        alert_id = 'https://alerts-v2.weather.gov/#/?id=' + evt.feature.properties['url'].split('products/')[1]
+        alert_id = evt.feature.properties['url']
        
         return L.Util.template(
         "<div class='container rounded-0' style='max-width:375px;margin-top:5px;'>" +
